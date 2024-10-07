284cc0d63e353bd4a9deaf89e5cdf7be1d976ffc
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-06 21:26:20 -0300

    reseting everything to use web

--- main.go
+++ main.go
@@ -1,33 +1,4 @@
 package main
 
-import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
-)
-
 func main() {
-	// Create a new application with a unique ID
-	myApp := app.NewWithID("whatever")
-	myWindow := myApp.NewWindow("PDF Tools")
-
-	//optimize form
-	optimizeForm := InitOptimizeForm(myWindow)
-	// Create the splitting form
-	splitForm := InitSplitForm(myWindow)
-
-	// Combine both forms in a vertical box
-	form1 := container.NewVBox(optimizeForm)
-	form2 := container.NewVBox(layout.NewSpacer(), splitForm)
-
-	tabs := container.NewAppTabs(
-		container.NewTabItem("Optimize a PDF file", container.NewPadded(form1)),
-		container.NewTabItem("Split a PDF file", form2),
-	)
-
-	myWindow.SetContent(tabs)
-
-	myWindow.Resize(fyne.NewSize(1000, 600))
-	myWindow.ShowAndRun()
 }
